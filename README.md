e63bfa7a34f0b08ff14e48de8fc7bc62228a867b
Anne Wirsig <annewirsig@yahoo.com>
2017-07-05 15:41:25 +0200

    Projekt ausgearbeitet, Dokumentation ergänzt, ReadMe geschrieben.

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Movie Trailer Website
 
-einfach ein Versuch zu schreiben
+This repository contains code for a simple website displaying the posters and playing the trailers of some of my favourite movies.
+
+# Source
+the main outside source-code is the fresh_tomatoes.py-file from ud036_StarterCode
+
+# Usage
+to see the website, download the repo and run entertainment_center.py
+
+# Files description:
+1. fresh_tomatoes.py: 
+	when passed a movie list to the function open_movies_page(), 
+	it builds the necessary html-file, opens a browser and displays the website
+
+2. media.py:
+	holds the definitions of the Video-class with the subclass Movie
+	every Movie-instance needs to be passed the movie title, movie storyline, the url of a movie poster and the url of a trailer of the movie on youtube upon initialisation
+
+3. entertainment_center.py:
+	contains the list with some of my favourite movies, builds the Movie Instances with the appropriate information 
+	and calls the open_movies_page()-function from fresh_tomatoes.py
+	- feel free to add movies (always appending the Movie()-instances to the global movies-variable)
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,55 +1,75 @@
 import media
 import fresh_tomatoes
-# -*- coding: utf-8 -*-
 
+# Build the Movie Instances and collect them in a list
 movies = []
-#Toy Story
-toy_story = media.Movie('Toy Story', 60,
-						'A story of a boy and his toys that come to life', 
-						'http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg', 
-						'https://www.youtube.com/watch?v=vwyZH85NQC4')
-						
-movies.append(toy_story)
-#Avatar
-avatar = media.Movie('Avatar', 60,
-					 'A marine on an alien planet', 
-					 'http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg', 
-					 'https://www.youtube.com/watch?v=5PSNL1qE6VY')
 
-movies.append(avatar)
+# Mask of Zorro
+zorro = media.Movie(
+                    'The Mask of Zorro',
+                    "A young thief, seeking revenge for the "
+                    "death of his brother, "
+                    "is trained by the once great, but aged Zorro, "
+                    "who also pursues vengeance of his own.",
+                    'https://images-na.ssl-images-amazon.com/images/M/MV5BNjI3NzI2MjY2MF5BMl5BanBnXkFtZTYwMDM5ODU5._V1_.jpg',  # NOQA
+                    'https://www.youtube.com/watch?v=FzrhTLNorF0'
+                    )
+movies.append(zorro)
 
-#Pride & Prejudice (1995)
-pride_and_prejudice = media.Movie('Pride & Prejudice', 360,
-					 "Jane Austen's classic novel about the prejudice that occurred between the 19th century classes and the pride which would keep lovers apart.", 
-					 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTU1MDY4OTU5OF5BMl5BanBnXkFtZTcwOTg5NzAyMw@@._V1_.jpg', 
-					 'https://www.youtube.com/watch?v=P5MmcT_vcBU')
+# Much Ado About Nothing
+much_ado = media.Movie(
+                    'Much Ado about nothing',
+                    'Young lovers Hero and Claudio, soon to wed, '
+                    'conspire to get verbal sparring partners and '
+                    'confirmed singles Benedick and Beatrice to wed as well.',
+                    'https://images-na.ssl-images-amazon.com/images/M/MV5BY2Q3YTU4OTgtZTc0My00YmY1LWJmOTQtMmMwNGE2ZjBmNmIxXkEyXkFqcGdeQXVyMTAwMzUyOTc@._V1_.jpg',  # NOQA
+                    'https://www.youtube.com/watch?v=wGlmhwa0zjw'
+                    )
+movies.append(much_ado)
 
-movies.append(pride_and_prejudice)
+# Taxi
+taxi = media.Movie(
+                    'Taxi',
+                    "To work off his tarnished driving record, "
+                    "a hip taxi driver must chauffeur a loser "
+                    "police inspector on the trail of German bank robbers.",
+                    'https://images-na.ssl-images-amazon.com/images/M/MV5BMmViOGVjZWQtZmNmNS00MWU3LWFjNGEtMDc1MjNlY2ZlNjdiXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SY1000_CR0,0,701,1000_AL_.jpg',  # NOQA
+                    'https://www.youtube.com/watch?v=8nANiVnDJOo'
+                    )
+movies.append(taxi)
 
-#Ladyhawke
-ladyhawke = media.Movie('Ladyhawke', 60,
-					 "CURSED FOR ETERNITY...No force in Heaven will release them. No power on Earth can save them.", 
-					 'https://images-na.ssl-images-amazon.com/images/M/MV5BYmYyNzBjNGUtOGYwZi00ZTVjLWE5YzYtZTcyMjFiMDExYmVkXkEyXkFqcGdeQXVyNjU0MTA3NzQ@._V1_SY1000_CR0,0,713,1000_AL_.jpg', 
-					 'https://www.youtube.com/watch?v=iGs4o_5jyhw')
-
+# Ladyhawke
+ladyhawke = media.Movie(
+                    'Ladyhawke',
+                    "CURSED FOR ETERNITY..."
+                    "No force in Heaven will release them. "
+                    "No power on Earth can save them.",
+                    'https://images-na.ssl-images-amazon.com/images/M/MV5BYmYyNzBjNGUtOGYwZi00ZTVjLWE5YzYtZTcyMjFiMDExYmVkXkEyXkFqcGdeQXVyNjU0MTA3NzQ@._V1_SY1000_CR0,0,713,1000_AL_.jpg',  # NOQA
+                    'https://www.youtube.com/watch?v=iGs4o_5jyhw'
+                    )
 movies.append(ladyhawke)
 
-#Drei Haselnuesse
-drei_haselnuesse = media.Movie('Drei Haselnuesse fuer Aschenputtel', 60,
-					 "Life changes dramatically for a Czech housemaid when the house driver gives her three magical hazel nuts.", 
-					 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMzg2Njc3NV5BMl5BanBnXkFtZTcwOTA1MTEzMQ@@._V1_.jpg', 
-					 'https://www.youtube.com/watch?v=bptf3Zlqlgo')
+# Black Beauty
+black_beauty = media.Movie(
+                    'Black Beauty',
+                    'A story for all ages. A Friendship for all time. '
+                    'Share the adventure.',
+                    'https://images-na.ssl-images-amazon.com/images/M/MV5BMjEwNTc3NzI4NF5BMl5BanBnXkFtZTcwOTk3MDcyMQ@@._V1_.jpg',  # NOQA
+                    'https://www.youtube.com/watch?v=-qCPMP4mNcQ'
+                    )
+movies.append(black_beauty)
 
-movies.append(drei_haselnuesse)
-
-#Secondhand Lions
-secondhand = media.Movie('Secondhand Lions', 60,
-					 "A coming-of-age story about a shy, young boy sent by his irresponsible mother to spend the summer with his wealthy, eccentric uncles in Texas.", 
-					 'https://upload.wikimedia.org/wikipedia/en/6/6a/SecondhandLions.jpg', 
-					 'https://www.youtube.com/watch?v=Ebkrm7u44UI')
+# Secondhand Lions
+secondhand = media.Movie('Secondhand Lions',
+                     "A coming-of-age story about a shy, "
+                     "young boy sent by his irresponsible mother to spend "
+                     "the summer with his wealthy, eccentric uncles in Texas.",
+                     'https://upload.wikimedia.org/wikipedia/en/6/6a/SecondhandLions.jpg',  # NOQA
+                     'https://www.youtube.com/watch?v=Ebkrm7u44UI')
 
 movies.append(secondhand)
 
-#fresh_tomatoes.open_movies_page(movies)
-print (secondhand.show_info())
-#print (media.Movie.__bases__)
+
+# Hand the Movie-List to the Fresh Tomatos-Open_movies_page-funtion
+# (aka build and open the Webpage)
+fresh_tomatoes.open_movies_page(movies)

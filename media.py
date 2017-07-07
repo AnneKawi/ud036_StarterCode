4413539ad3876c8d5190aa239a028c367efb3a81
Anne Wirsig <annewirsig@yahoo.com>
2017-07-07 12:36:12 +0200

    Media.py: auf Pep8 angepasst.

--- media.py
+++ media.py
@@ -1,40 +1,46 @@
 import webbrowser
 
-# A superclass about Videos
-class Video():
-    '''a class for vieos. Currently only with attribute title (str).'''
-    
+
+class Video():  # A superclass about Videos
+
+    '''a class for videos. Currently only with attribute title (str).'''
+
     def __init__(self, title):
         self.title = title
-        
-    def show_info(self): # print the title 
+
+    def show_info(self):  # print the title
         print (self.title)
 
-# The Movie-Class (inheriting Video) with the title, storyline, URLs to the poster and the youtube trailer      
-class Movie(Video):
+
+class Movie(Video):  # The Movie-Class (inheriting Video)
+
     '''a class for movies.
-    
+
        inherits Video, from which inherits the attribute title
-       
-       additionally keeps the attributes: 
+
+       additionally keeps the attributes:
             storyline (str): the storyline of the title
-            poster_image_url (str): url pointing to a picture poster of the movie
-            trailer_youtube_url (str): url pointing to the trailer of the video on youtube 
-        
-        movie_title, movie_storyline, poster_image_url and trailer_youtube_url need to be passed in on initialisation of any class instance
-            
+            poster_image_url (str): url pointing to a picture poster
+                                    of the movie
+            trailer_youtube_url (str): url pointing to the trailer of
+                                       the video on youtube
+
+        movie_title, movie_storyline, poster_image_url and trailer_youtube_url
+        need to be passed in on initialisation of any class instance
+
         The function show_trailer opens the trailer in a browser.
         The function show_info prints title and storyline.
     '''
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         Video.__init__(self, movie_title)
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        
-    def show_trailer(self): # open a Browser and show the trailer
+
+    def show_trailer(self):  # open a Browser and show the trailer
         webbrowser.open(self.trailer_youtube_url)
 
-    def show_info(self): # print the Title and the Storyline
+    def show_info(self):  # print the Title and the Storyline
         print (self.title)
         print (self.stoyline)
